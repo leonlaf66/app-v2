e6a6190cf656e3551c982ffcb2929116918c0f9d
LiChunHua <lichunhua@medatc.com>
2018-01-20 22:41:47 +0800

    x

---
+++ src/components/news/list.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="news-list">
+    <a class="news-item vux-1px-b" v-for="item in items" @click="$emit('selected', item.id)" :key="item.id">
+      <div class="item-inner">
+        <div class="image" :style="item.image_url | image-style">
+        </div>
+        <div class="extends">
+          <h2>{{ item.title }}</h2>
+          <div class="info">阅读 {{ item.hits }} 收藏 0</div>
+          <div class="public-at">{{ item.publish_at.substring(0, 16)}}</div>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'news-list',
+  props: ['items'],
+  filters: {
+    imageStyle (url) {
+      return `background-image:url(${url})`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.news-list {
+  background-color:#fff;
+  .news-item {
+    display:block;
+    padding:10px 7px;
+    &:active {
+      background:#f6f6f6;
+    }
+  }
+  .item-inner {
+    position:relative;
+    height:100px;
+    border-radius: 4px;
+  }
+  .image, .extends {position:absolute;top:0;}
+  .image {
+    left:0;
+    height:100%;width:140px;
+    background-size: cover;
+    background-position: center center;
+  }
+  .extends {
+    height:100%;
+    left: 150px;right:0;
+    > h2 {
+      margin:0;padding:0;font-size:14px;
+    }
+  }
+  .public-at {
+    position:absolute;
+    bottom:0;
+    color:#777;
+  }
+  .info {
+    position:absolute;
+    bottom:0;
+    right:0;
+    color:#777;
+  }
+}
+</style>
---
+++ src/pages/news/view.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="page-news-view has-fixed-header">
+    <x-header class="fixed"></x-header>
+    <div class="news-view">
+      <div class="view-head">
+        <h1>{{ data.title }}</h1>
+        <div class="date">
+          <i class="iconfont icon-time"></i>
+          {{ data.publish_at.substring(0, 16) }}
+        </div>
+      </div>
+      <div v-html="data.content" class="view-body">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import XHeader from 'vux/src/components/x-header'
+
+export default {
+  data () {
+    return {
+      data: {
+        publish_at: ''
+      }
+    }
+  },
+  created () {
+    this.$store.dispatch('loading', true)
+    this.load(this.$route.params.id).then(data => {
+      this.$setPageTitle(data.title)
+      this.data = data
+      this.$store.dispatch('loading', false)
+    })
+  },
+  methods: {
+    load (id) {
+      return this.$newsApi(`item/${id}`)
+    }
+  },
+  destroyed () {
+    this.data = {
+      publish_at: ''
+    }
+  },
+  components: {
+    XHeader
+  }
+}
+</script>
+
+<style lang="scss">
+.page-news-view {
+  .news-view {
+    .view-head {
+      > h1 {
+        padding:35px 0 20px 0;
+        text-align:center;
+        font-size:18px;
+      }
+      .date {
+        text-align:center;
+        margin-top:-15px;
+      }
+    }
+    .view-body {
+      padding:10px;
+      margin-bottom: 20px;
+      line-height: 20px;
+      h1, h2, h3, h4, h5, h6 {
+        position: relative;
+        margin-top: 1em;
+        margin-bottom: 16px;
+        font-weight: 700;
+        line-height: 1.4;
+      }
+      p {
+          margin:15px 0;
+          line-height: 20px;
+      }
+      table {
+        border-collapse: collapse;
+        border-spacing: 0;
+        display: block;
+        width: 100%;
+        overflow: auto;
+        word-break: normal;
+        word-break: keep-all;
+        th {
+          background: #e8e8e8;
+          font-weight: bold;
+        }
+        th, td {
+          padding: 5px 10px;
+          border: 1px solid #d9d9d9;
+          text-align: left !important;
+        }
+      }
+      img {
+        display:block;
+        margin-left:auto;
+        margin-right:auto;
+        max-width:100%;
+      }
+      ul, ol {
+        margin-left:15px;
+      }
+      blockquote {
+        padding: 0 15px;
+        color: #777;
+        border-left: 4px solid #ddd;
+      }
+    }
+  }
+}
+</style>
