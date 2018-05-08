abd9ca39b76b6b1dff660f9f210cc1a0c0548a06
LiChunHua <lichunhua@medatc.com>
2018-05-08 19:12:09 +0800

    x

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: 'http://cdn.webapp.usleju.com/',
+    assetsPublicPath: 'http://webappcdn.usleju.com',
 
     /**
      * Source Maps
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   GOOGLE_MAP_KEY: '"AIzaSyAKfreY6Rf9fOvsJNcxGMsPPNHpXbvj6wo"',
-  MEDIA_BASE_URL: '"http://media.usleju.cn"',
+  MEDIA_BASE_URL: '"http://mediacdn.usleju.com"',
   API_HOUSE_URL: '"http://172.104.15.126:8001"',
   API_YP_URL: '"http://172.104.15.126:8002"',
   API_NEWS_URL: '"http://172.104.15.126:8003"',
