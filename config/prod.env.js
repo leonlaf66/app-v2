9d39f484320099759e39b164613caf70c15d05f8
LiChunHua <lichunhua@medatc.com>
2018-05-08 21:39:27 +0800

    x

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,10 +3,10 @@ module.exports = {
   NODE_ENV: '"production"',
   GOOGLE_MAP_KEY: '"AIzaSyAKfreY6Rf9fOvsJNcxGMsPPNHpXbvj6wo"',
   MEDIA_BASE_URL: '"http://mediacdn.usleju.com"',
-  API_HOUSE_URL: '"http://172.104.15.126:8001"',
-  API_YP_URL: '"http://172.104.15.126:8002"',
-  API_NEWS_URL: '"http://172.104.15.126:8003"',
-  API_PASSPORT_URL: '"http://172.104.15.126:8004"',
-  API_MEMBER_URL: '"http://172.104.15.126:8005"',
-  API_COMMENT_URL: '"http://172.104.15.126:8050"'
+  API_HOUSE_URL: '"http://119.23.14.17:8001"',
+  API_YP_URL: '"http://119.23.14.17:8002"',
+  API_NEWS_URL: '"http://119.23.14.17:8003"',
+  API_PASSPORT_URL: '"http://119.23.14.17:8004"',
+  API_MEMBER_URL: '"http://119.23.14.17:8005"',
+  API_COMMENT_URL: '"http://119.23.14.17:8050"'
 }
