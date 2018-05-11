ff3fe30f54b23c67b21a125b2c01c36e09fca1b5
LiChunHua <lichunhua@medatc.com>
2018-05-11 08:01:53 +0800

    x

--- src/pages/house/workflow.vue
+++ src/pages/house/workflow.vue
@@ -2,7 +2,7 @@
   <div class="page-buy-house-flow has-fixed-header">
     <x-header class="fixed" :left-options="{backText: $tt('RETURN', '返回')}">
     </x-header>
-    <img class="flows" src="/static/imgs/myflow.png">
+    <img class="flows" :src="myflowUrl">
   </div>
 </template>
 
@@ -11,6 +11,11 @@ import XHeader from 'vux/src/components/x-header'
 
 export default {
   name: 'buy-house-flow',
+  computed: {
+    myflowUrl () {
+      return process.env.MEDIA_BASE_URL + '/imgs/myflow.png'
+    }
+  },
   components: {
     XHeader
   }
